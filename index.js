117426cddd1392945e3349a11f99b3530bf2e06b
lisiurday <lisiurday@gmail.com>
2017-05-06 08:54:48 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   U: require('./src/utils'),
-  C: require('./src/classes')
+  C: require('./src/classes'),
   prop: require('./src/prop'),
   propWithDefault: require('./src/propWithDefault')
 }
